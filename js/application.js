4a7f320873c952ff8256e336a2964a90d4badd1c
Chebichii-Lab <natasha.jebichii@student.moringaschool.com>
2021-04-29 10:02:48 +0300

    update the application file

---
+++ js/application.js
@@ -0,0 +1,24 @@
+function haveSubmissions() {
+    let firstName= document.getElementById("fname").value;
+    let middleName = document.getElementById("mname").value;
+    let lastName = document.getElementById("lname").value;
+    let birthDay = document.getElementById("birthday").checked;
+    let female = document.getElementById("female").value;
+    let male = document.getElementById("male").value;
+    let county = document.getElementById("county").value;
+    let country = document.getElementById("country").value;
+    let high = document.getElementById("high").checked;
+    let dateMax = document.getElementById("datemax").value;
+    let dateMin = document.getElementById("datemin").value;
+    let grade = document.getElementById("grade").value;
+    let university = document.getElementById("university").checked;
+    let bursaryOne = document.getElementById("bursary1").checked;
+
+
+    if(firstName === "" || middleName === "" || lastName === "" || birthDay === "" || female === "" || male === "" || county === "" || country === "" || high === "" || dateMax === "" || dateMin === "" || grade === "" ||
+    university === "" || bursaryOne === ""){
+        alert("Please fill in fields")
+    } else {
+        alert("Submitted")
+    }
+}
